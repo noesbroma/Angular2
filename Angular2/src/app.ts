f9395410bdc846888cfc2669894d1ebf9e06b306
Judit <noesbroma@hotmail.com>
2016-11-26 12:36:36 +0100

    init

---
+++ Angular2/src/app.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from "angular2/core"
+import { MyModel } from "./model"
+
+@Component({
+    selector: 'my-app',
+    template: '<div>{{titulo}}</div>'
+})
+export class MyApp {
+    public titulo = "HOLA MUNDO!!!!"
+    model = new MyModel();
+    getCompiler() {
+        return this.model.compiler;
+    }  
+}
